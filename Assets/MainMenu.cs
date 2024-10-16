a2c38368f32fef05d4ba0c22cb98b65269a9f0c0
Hadesss00 <162340170+Hadesss00@users.noreply.github.com>
2024-10-16 14:37:16 +0800

    AnimationIdle

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -5,9 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void InstructionsGame()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void CreditsMenu()
+    {
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
